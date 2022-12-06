3ec7137c26c99d67abbe111113eb4883e037c62d
WizardofHause <jonrhause@gmail.com>
2022-12-05 19:02:39 -0700

    added byebug gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-    # skip_before_action :verify_authenticity_token
+    skip_before_action :verify_authenticity_token
     include ActionController::Cookies
     rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
     rescue_from ActiveRecord::RecordInvalid, with: :unprocessable_entity_response
